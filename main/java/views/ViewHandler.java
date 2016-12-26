c648871d650f57bf3c20b31d48c67744f0ff65c6
Jatin Yadav <Jatin.Yadav1307@gmail.com>
2016-12-26 22:50:11 +0530

    Added ViewHandler to repository

---
+++ main/java/views/ViewHandler.java
@@ -0,0 +1,6 @@
+package views;
+
+public final class ViewHandler {
+    public static final LoginWindow loginWindow = new LoginWindow();
+    public static final SignupWindow signupWindow =  new SignupWindow();
+}
