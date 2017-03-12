51c45e712ee7800b25d35d5de1ae4c0c26a4f28a
Jatin Yadav <Jatin.Yadav1307@gmail.com>
2017-03-12 22:55:41 +0530

    Added counter for number of times to save the input

--- main/java/controllers/staticAuth/TrainingWindowController.java
+++ main/java/controllers/staticAuth/TrainingWindowController.java
@@ -1,30 +1,25 @@
 package controllers.staticAuth;
 
-import com.mongodb.operation.UpdateOperation;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
-import models.Cookie;
-import models.Keystroke;
 import models.Training;
 import models.User;
 import mongoConnection.Connection;
 import mongoConnection.ConnectionHandler;
-import org.joda.time.DateTime;
-import org.mongodb.morphia.Key;
 import org.mongodb.morphia.query.Query;
 import org.mongodb.morphia.query.UpdateOperations;
 import supportClass.KeyStrokeDataValue;
 import views.Universal;
+import views.ViewHandler;
 
 import java.time.Instant;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.List;
 
 public class TrainingWindowController {
+    private int submitButtonCount = 1;
     private ArrayList<Long> pressTimes = new ArrayList<>(0);
     private ArrayList<Long> releaseTimes = new ArrayList<>(0);
     private ArrayList<String> characters = new ArrayList<>(0);
@@ -79,20 +74,20 @@ public class TrainingWindowController {
         UpdateOperations<User> updateLastTrainingSession =  connection.getDatastore().createUpdateOperations(User.class)
                 .set("lastTrainingSession", Date.from(Instant.now()));
         UpdateOperations<User> updateNextTrainingSession =  connection.getDatastore().createUpdateOperations(User.class)
-                .set("nextTrainingSession", Date.from(Instant.now().plusSeconds(150 * Universal.currentUser.getNextSessionNumber())));
-
+                .set("nextTrainingSession", Date.from(Instant.now().plusSeconds(15 * Universal.currentUser.getNextSessionNumber())));
+        // UPDATE THE MULTIPLIER TO 150 for 15 MINUTES gap between training!!
         connection.getDatastore().update(query, updateTrainingNumber);
         connection.getDatastore().update(query, updateLastTrainingSession);
         connection.getDatastore().update(query, updateNextTrainingSession);
         for (Training newData : trainingData)
             connection.getDatastore().save(newData);
         System.out.println("Saved!");
-
-//        -------------------- Coookie TESTING -----------------------------
-//        Query<Cookie> query = connection.getDatastore().createQuery(Cookie.class)
-//                .field("relatesTo").equal(Universal.currentUser.getObjectId());
-//        List<Cookie> results = query.asList();
-//        System.out.println(results.get(0).getRelatesTo());
+        submitButtonCount++;
+        if (submitButtonCount > 2)
+        {
+            ViewHandler.trainingWindow.getTrainingStage().close();
+            submitButtonCount = 1;
+        }
     }
 
     public void clearButton(ActionEvent event, TextField textField) {
